e63910d085b9d98b473d07c29fd33c0d80d04627
Jonathan Guyer <guyer@nist.gov>
2018-02-24 09:39:10 -0500

    Add sumatra tag

--- threadtest.sh
+++ threadtest.sh
@@ -6,4 +6,4 @@ NTHREADS=$(( 2**(SGE_TASK_ID-1) ))
 NCPUS=$(( NSLOTS / NTHREADS ))
 
 export OMP_NUM_THREADS=$NTHREADS
-mpiexec -n 1 smt run -n $NCPUS --main threadtest.py params.yaml nthreads=$NTHREADS ncpus=$NCPUS nslots=$NSLOTS $@
+mpiexec -n 1 smt run --tag threads -n $NCPUS --main threadtest.py params.yaml nthreads=$NTHREADS ncpus=$NCPUS nslots=$NSLOTS $@
