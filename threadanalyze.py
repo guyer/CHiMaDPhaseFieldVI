81a6ee2cdac6061f09b6b133c99441cdd8064505
Jonathan Guyer <guyer@nist.gov>
2018-02-25 18:35:57 -0500

    Convert script to use load_sumatreant

--- threadanalyze.py
+++ threadanalyze.py
@@ -1,39 +1,17 @@
-import pandas as pd
-from sumatra.projects import load_project
-import datreant.core as dtr
-
 import matplotlib
 matplotlib.use('TkAgg') 
 from matplotlib import pyplot as plt
 
+from sumatreant import load_sumatreant
 
-def parameters2columns(parameters):
-    import yaml
+df = load_sumatreant(project_name='benchmark7')
 
-    d_orig = yaml.load(parameters['content'])
-    return pd.Series(d_orig)
+df = df[df['tags'].map(lambda x: '_finished_' in x and 'threads' in x)]
 
-project = load_project('.')
-df = pd.read_json(project.record_store.export('benchmark7'),
-                  convert_dates=["timestamp"])
-df = df.set_index(['label'])
+# print df[['label', 'timestamp', '--ncpus', '--nthreads', '--nslots', '--nx', 'duration', 'solvetime']] 
 
-df2 = df.merge(df.parameters.apply(parameters2columns), left_index=True, right_index=True)
-
-data = dtr.Treant('Data/')
-
-data = data[list(df2.index)]
-
-df3 = pd.DataFrame(index=data.names, data=data.bundle.categories.any)
-
-df4 = df2.merge(df3, left_index=True, right_index=True)
-
-df4 = df4[df4['tags'].map(lambda x: '_finished_' in x and 'threads' in x)]
-
-# print df4[['label', 'timestamp', '--ncpus', '--nthreads', '--nslots', '--nx', 'duration', 'solvetime']] 
-
-slots16 = df4[df4['nslots'] == 16]
-slots32 = df4[df4['nslots'] == 32]
+slots16 = df[df['nslots'] == 16]
+slots32 = df[df['nslots'] == 32]
 
 plt.plot(slots16['nthreads'], slots16['duration'], linestyle="", marker="x", color='blue')
 plt.plot(slots16['nthreads'], slots16['solvetime'], linestyle="", marker="+", color='blue')
