6664e63d4bf3a7daaa2dac2c5a2946a7ab32111e
Jonathan Guyer <guyer@nist.gov>
2018-02-25 09:45:03 -0500

    Make frame mergers more general

--- threadanalyze.py
+++ threadanalyze.py
@@ -11,32 +11,28 @@ def parameters2columns(parameters):
     import yaml
 
     d_orig = yaml.load(parameters['content'])
-    d = dict()
-    for k, v in d_orig.iteritems():
-        d["--" + k] = v
-    return pd.Series(d)
+    return pd.Series(d_orig)
 
 project = load_project('.')
 df = pd.read_json(project.record_store.export('benchmark7'),
                   convert_dates=["timestamp"])
-
-df = df[df['tags'].map(lambda x: '_finished_' in x)][['dependencies', 'diff', 'label', 'timestamp', 'duration', 'tags', 'version', 'parameters']]
+df = df.set_index(['label'])
 
 df2 = df.merge(df.parameters.apply(parameters2columns), left_index=True, right_index=True)
 
-df2 = df2[df2['tags'].map(lambda x: 'threads' in x)]
-
 data = dtr.Treant('Data/')
 
-data = data[list(df2['label'])]
+data = data[list(df2.index)]
 
-df3 = pd.DataFrame(index=data.names, data={'solvetime': data.bundle.categories['solvetime']})
+df3 = pd.DataFrame(index=data.names, data=data.bundle.categories.any)
 
-df4 = df2.merge(df3, left_on='label', right_index=True)
+df4 = df2.merge(df3, left_index=True, right_index=True)
+
+df4 = df4[df4['tags'].map(lambda x: '_finished_' in x and 'threads' in x)]
 
 # print df4[['label', 'timestamp', '--ncpus', '--nthreads', '--nslots', '--nx', 'duration', 'solvetime']] 
 
-plt.plot(df4['--nthreads'], df4['duration'], linestyle="", marker="x")
-plt.plot(df4['--nthreads'], df4['solvetime'], linestyle="", marker="+")
+plt.plot(df4['nthreads'], df4['duration'], linestyle="", marker="x")
+plt.plot(df4['nthreads'], df4['solvetime'], linestyle="", marker="+")
 
 plt.show()
