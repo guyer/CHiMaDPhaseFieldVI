cdb4460098863e00d9ad0d88d58ceae1f8da679a
Jonathan Guyer <guyer@nist.gov>
2018-02-25 19:23:58 -0500

    Switch from bash math to awk
    bash was doing integer math and bc isn't available on cluster

--- timestep.sh
+++ timestep.sh
@@ -3,4 +3,7 @@ source activate fipy
 cd /data/guyer/CHiMaDPhaseFieldVI
 
 export OMP_NUM_THREADS=1
-mpiexec -n 1 smt run --tag timestep -n $NSLOTS --main benchmark7a.py params.yaml  dt=1.e-$(( SGE_TASK_ID / 2 )) $@
+
+exp=$(awk "BEGIN {print $SGE_TASK_ID / 2}")
+
+mpiexec -n 1 smt run --tag timestep -n $NSLOTS --main benchmark7a.py params.yaml  dt=1.e-${exp} $@
