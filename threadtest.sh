873842924e88d08afc244316a77d9ff25f790845
Jonathan Guyer <guyer@nist.gov>
2018-02-26 09:10:08 -0500

    Change script to run fixed # MPI processes with varying # threads
    Hard-coded at 4 MPI processes

--- threadtest.sh
+++ threadtest.sh
@@ -2,8 +2,8 @@
 source activate fipy
 cd /data/guyer/CHiMaDPhaseFieldVI
 
-NTHREADS=$(( 2**(SGE_TASK_ID-1) ))
-NCPUS=$(( NSLOTS / NTHREADS ))
+NTHREADS=$SGE_TASK_ID
+NCPUS=4
 
 export OMP_NUM_THREADS=$NTHREADS
 mpiexec -n 1 smt run --tag threads -n $NCPUS --main threadtest.py params.yaml nthreads=$NTHREADS ncpus=$NCPUS nslots=$NSLOTS $@
