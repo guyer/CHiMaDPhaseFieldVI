aa8198962baf341bd780123ae7c1d43f2933a0a9
Jonathan Guyer <guyer@nist.gov>
2018-02-25 19:34:56 -0500

    Change calculation to python

--- timestep.sh
+++ timestep.sh
@@ -4,6 +4,6 @@ cd /data/guyer/CHiMaDPhaseFieldVI
 
 export OMP_NUM_THREADS=1
 
-exp=$(awk "BEGIN {print $SGE_TASK_ID / 2}")
+dt=$(python -c "print 10**($SGE_TASK_ID / 2.)")
 
-mpiexec -n 1 smt run --tag timestep -n $NSLOTS --main benchmark7a.py params.yaml  dt=1.e-${exp} $@
+mpiexec -n 1 smt run --tag timestep -n $NSLOTS --main benchmark7a.py params.yaml  dt=$dt $@
