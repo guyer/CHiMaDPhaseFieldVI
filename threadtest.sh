ca021521853a88914486c1ad7d99104ae78456e6
Jonathan Guyer <guyer@nist.gov>
2018-02-27 13:41:57 -0500

    Move mprof after mpiexec

--- threadtest.sh
+++ threadtest.sh
@@ -6,4 +6,4 @@ NTHREADS=$SGE_TASK_ID
 NCPUS=4
 
 export OMP_NUM_THREADS=$NTHREADS
-mprof run --include-children --multiprocess mpiexec -n 1 smt run --tag threads -n $NCPUS --main threadtest.py params.yaml nthreads=$NTHREADS ncpus=$NCPUS nslots=$NSLOTS $@
+mpiexec -n 1 mprof run --include-children --multiprocess smt run --tag threads -n $NCPUS --main threadtest.py params.yaml nthreads=$NTHREADS ncpus=$NCPUS nslots=$NSLOTS $@
