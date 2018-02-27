c0a64b6a200f29eb7ab6d4b1c0e89813949c9fce
Jonathan Guyer <guyer@nist.gov>
2018-02-27 13:44:15 -0500

    Distinguish arguments from parameters

--- threadtest.sh
+++ threadtest.sh
@@ -6,4 +6,4 @@ NTHREADS=$SGE_TASK_ID
 NCPUS=4
 
 export OMP_NUM_THREADS=$NTHREADS
-mpiexec -n 1 mprof run --include-children --multiprocess smt run --tag threads -n $NCPUS --main threadtest.py params.yaml nthreads=$NTHREADS ncpus=$NCPUS nslots=$NSLOTS $@
+mpiexec -n 1 mprof run --include-children --multiprocess -- smt run --tag threads -n $NCPUS --main threadtest.py params.yaml nthreads=$NTHREADS ncpus=$NCPUS nslots=$NSLOTS $@
