518251ec17d27f3b1a283fdf6cf42f13497cc12c
Jonathan Guyer <guyer@nist.gov>
2018-02-28 10:37:04 -0500

    Add script to vary grid size for benchmark7a

---
+++ meshsize.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+source activate fipy
+cd /data/guyer/CHiMaDPhaseFieldVI
+
+export OMP_NUM_THREADS=1
+
+nx=`python -c "print 100 * 2**($SGE_TASK_ID - 1)"`
+
+if [ "$SGE_TASK_ID" == "undefined" ]; then
+    OUTPUT="${JOB_ID}"
+else
+    OUTPUT="${JOB_ID}.${SGE_TASK_ID}"
+fi
+
+OUTPUT="mprofile_${OUTPUT}"
+
+mpiexec -n 1 mprof run --include-children --multiprocess --output $OUTPUT -- smt run --tag meshsize -n $NSLOTS --main benchmark7a.py params.yaml  nx=$nx dt=1.e-4 nthreads=$OMP_NUM_THREADS ncpus=$NSLOTS nslots=$NSLOTS $@
