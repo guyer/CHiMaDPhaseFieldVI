c3d01a4b9499a4eb4fc27ecd4fd8301ab14f7fc8
Jonathan Guyer <guyer@nist.gov>
2018-02-28 10:51:23 -0500

    Remove erroneous '--' that argparse doesn't accept

--- meshsize.sh
+++ meshsize.sh
@@ -14,4 +14,4 @@ fi
 
 OUTPUT="mprofile_${OUTPUT}"
 
-mpiexec -n 1 mprof run --include-children --multiprocess --output $OUTPUT -- smt run --tag meshsize -n $NSLOTS --main benchmark7a.py params.yaml  nx=$nx dt=1.e-4 nthreads=$OMP_NUM_THREADS ncpus=$NSLOTS nslots=$NSLOTS $@
+mpiexec -n 1 mprof run --include-children --multiprocess --output $OUTPUT smt run --tag meshsize -n $NSLOTS --main benchmark7a.py params.yaml  nx=$nx dt=1.e-4 nthreads=$OMP_NUM_THREADS ncpus=$NSLOTS nslots=$NSLOTS $@
