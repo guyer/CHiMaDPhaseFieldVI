3932cb4f4324db0d53c7a7e96d64b2b7ac9bdd66
Jonathan Guyer <guyer@nist.gov>
2018-03-06 16:43:43 -0500

    Enforce float division
    The symbolic form has 1/2 in it, which SymPy knows is 0.5, but
    when we eval the string in the subsidiary script, we get integer
    division

--- leaker7a.py
+++ leaker7a.py
@@ -1,6 +1,11 @@
 # script based on 
 # https://pages.nist.gov/pfhub/benchmarks/benchmark7.ipynb
 
+# sympy knows that 1/2 is 0.5, but by the time we're eval'ing
+# eta_str, that information is lost
+
+from __future__ import division
+
 import os
 import pickle
 import sys
