2958b29d66d0a847dcb86067e92af96b11099c5f
Jonathan Guyer <guyer@nist.gov>
2018-03-08 16:31:29 -0500

    Change timestep.sh to work like meshsize.sh
    Use `initializer7a.py` and `leaker7a.py`

--- timestep.sh
+++ timestep.sh
@@ -6,4 +6,12 @@ export OMP_NUM_THREADS=1
 
 dt=$(python -c "print 10**-($SGE_TASK_ID / 2.)")
 
-mpiexec -n 1 smt run --tag timestep -n $NSLOTS --main benchmark7a.py params.yaml  dt=$dt $@
+if [ "$SGE_TASK_ID" == "undefined" ]; then
+    OUTPUT="${JOB_ID}"
+else
+    OUTPUT="${JOB_ID}.${SGE_TASK_ID}"
+fi
+
+OUTPUT="mprofile_${OUTPUT}"
+
+mprof run --include-children --multiprocess --output $OUTPUT smt run --tag timestep --main initializer7a.py params.yaml nx=100 dt=$dt checkpoint=1.e-1 nthreads=$OMP_NUM_THREADS ncpus=$NSLOTS nslots=$NSLOTS nproc=$NSLOTS $@
