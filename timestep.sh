1ebac1e24b992c1073e0ccf75e27060d10bc1fbb
Jonathan Guyer <guyer@nist.gov>
2018-03-08 23:00:35 -0500

    Decrease mesh spacing

--- timestep.sh
+++ timestep.sh
@@ -14,4 +14,4 @@ fi
 
 OUTPUT="mprofile_${OUTPUT}"
 
-mprof run --include-children --multiprocess --output $OUTPUT smt run --tag timestep --main initializer7a.py params.yaml nx=100 dt=$dt checkpoint=1.e-1 nthreads=$OMP_NUM_THREADS ncpus=$NSLOTS nslots=$NSLOTS nproc=$NSLOTS $@
+mprof run --include-children --multiprocess --output $OUTPUT smt run --tag timestep --main initializer7a.py params.yaml nx=800 dt=$dt checkpoint=1.e-1 nthreads=$OMP_NUM_THREADS ncpus=$NSLOTS nslots=$NSLOTS nproc=$NSLOTS $@
