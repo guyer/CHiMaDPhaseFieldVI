4aec779d3c019b3594bb1542d2b3003fc7569e8b
Jonathan Guyer <guyer@nist.gov>
2018-03-16 14:59:25 -0400

    Remove memory profiles if Sumatra refused to run

--- archive_mprofile.sh
+++ archive_mprofile.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+changed="Code has changed, please commit your changes"
+
 for f in mprofile_*
 do
-    if label=$(./find_sumatra_for_jobid.sh ${f/mprofile_/}); then
+    jobid=${f/mprofile_/}
+    if label=$(./find_sumatra_for_jobid.sh $jobid); then
 	if [ "$label" != "" ]; then
 	    echo mv $f Data/${label}/mprofile.dat
-	    mv $f Data/${label}/mprofile.dat	    
+	    mv $f Data/${label}/mprofile.dat
+	elif grep --quiet "$changed" qsublogs/*.o${jobid}; then
+	    echo rm $f
+	    rm $f
 	fi
     fi
 done
