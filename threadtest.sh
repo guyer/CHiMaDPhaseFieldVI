cf317d87508d956357724ba9ae834877b23f3b37
Jonathan Guyer <guyer@nist.gov>
2019-12-27 10:03:31 -0500

    Switch to SLURM syntax from SGE

--- threadtest.sh
+++ threadtest.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-source activate fipy
-cd /data/guyer/CHiMaDPhaseFieldVI
+# invoke with
+# $ sbatch --array=0-5 --ntasks=32 threadtest.sh
 
-NTHREADS=$(( 2**(SGE_TASK_ID-1) ))
+#SBATCH --job-name=threads
+#SBATCH --workdir=/data/guyer/CHiMaDPhaseFieldVI
+
+source activate petsc3k
+
+NTHREADS=$(( 2**SLURM_ARRAY_TASK_ID ))
+NSLOTS=$SLURM_NTASKS
 NCPUS=$(( NSLOTS / NTHREADS ))
 
-export OMP_NUM_THREADS=$NTHREADS
-mpiexec -n 1 smt run --tag threads -n $NCPUS --main threadtest.py params.yaml nthreads=$NTHREADS ncpus=$NCPUS nslots=$NSLOTS $@
+echo export OMP_NUM_THREADS=$NTHREADS
+echo mpiexec -n 1 smt run --tag threads -n $NCPUS --main threadtest.py params.yaml nthreads=$NTHREADS ncpus=$NCPUS nslots=$NSLOTS $@
