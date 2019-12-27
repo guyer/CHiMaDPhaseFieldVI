41b42371cfb77521a4d828b70f24962f32cbdd9c
Jonathan Guyer <guyer@nist.gov>
2019-12-27 09:25:44 -0500

    Revert to version that traded threads for cpus

--- threadtest.sh
+++ threadtest.sh
@@ -2,8 +2,8 @@
 source activate fipy
 cd /data/guyer/CHiMaDPhaseFieldVI
 
-NTHREADS=$SGE_TASK_ID
-NCPUS=4
+NTHREADS=$(( 2**(SGE_TASK_ID-1) ))
+NCPUS=$(( NSLOTS / NTHREADS ))
 
 export OMP_NUM_THREADS=$NTHREADS
-mpiexec -n 1 mprof run --include-children --multiprocess -- smt run --tag threads -n $NCPUS --main threadtest.py params.yaml nthreads=$NTHREADS ncpus=$NCPUS nslots=$NSLOTS $@
+mpiexec -n 1 smt run --tag threads -n $NCPUS --main threadtest.py params.yaml nthreads=$NTHREADS ncpus=$NCPUS nslots=$NSLOTS $@
