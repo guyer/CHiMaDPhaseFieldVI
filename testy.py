51af14bed4dad58bfc36750a866110431a128bd2
Jonathan Guyer <guyer@nist.gov>
2019-12-28 19:43:46 -0500

    Add diagnostic script

---
+++ testy.py
@@ -0,0 +1,16 @@
+import os
+import yaml
+
+yamlfile = sys.argv[1]
+
+with open(yamlfile, 'r') as f:
+    params = yaml.load(f)
+
+from sumatra.projects import load_project
+project = load_project(os.getcwd())
+record = project.get_record(params["sumatra_label"])
+output = record.datastore.root
+
+with open(os.path.join(output, "file.txt"), 'w') as f:
+    f.write("eeny")
+
