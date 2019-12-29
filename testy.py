adbb9e59ca5188f8d3b391487479c6ee57093ac3
Jonathan Guyer <guyer@nist.gov>
2019-12-28 20:24:55 -0500

    Change diagnostics

--- testy.py
+++ testy.py
@@ -7,13 +7,12 @@ yamlfile = sys.argv[1]
 with open(yamlfile, 'r') as f:
     params = yaml.load(f)
 
-raise Exception(str(params))
-
 from sumatra.projects import load_project
 project = load_project(os.getcwd())
+raise Exception(str(project))
 record = project.get_record(params["sumatra_label"])
+raise Exception(str(record))
 output = record.datastore.root
-
 raise Exception(output)
 
 with open(os.path.join(output, "file.txt"), 'w') as f:
