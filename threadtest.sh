44bf585cbe10ba534c60a85c50a03179580b4e08
Jonathan Guyer <guyer@nist.gov>
2019-12-29 18:08:13 -0500

    Activate slurm script

--- threadtest.sh
+++ threadtest.sh
@@ -5,11 +5,11 @@
 #SBATCH --job-name=threads
 #SBATCH --workdir=/data/guyer/CHiMaDPhaseFieldVI
 
-source activate petsc3k
+source activate petsc37
 
 NTHREADS=$(( 2**SLURM_ARRAY_TASK_ID ))
 NSLOTS=$SLURM_NTASKS
 NCPUS=$(( NSLOTS / NTHREADS ))
 
-echo export OMP_NUM_THREADS=$NTHREADS
-echo mpiexec -n 1 smt run --tag threads2 -n $NCPUS --main threadtest.py params.yaml nthreads=$NTHREADS ncpus=$NCPUS nslots=$NSLOTS $@
+export OMP_NUM_THREADS=$NTHREADS
+FIPY_SOLVERS=petsc /data/guyer/miniconda3/envs/petsc37/bin/mpiexec -n 1 smt run --tag threads2 --tag petsc -n $NCPUS --main threadtest.py params.yaml nthreads=$NTHREADS ncpus=$NCPUS nslots=$NSLOTS $@
