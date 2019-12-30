026c266e7f74cef8f0023cc58c7638cacc5543b4
Jonathan Guyer <guyer@nist.gov>
2019-12-30 16:44:40 -0500

    Remove echo

--- threadtest.sh
+++ threadtest.sh
@@ -19,4 +19,4 @@ NCPUS=$(( NSLOTS / NTHREADS ))
 
 
 export OMP_NUM_THREADS=$NTHREADS
-echo FIPY_SOLVERS=$SOLVER /data/guyer/miniconda3/envs/$CONDAENV/bin/mpiexec -n 1 smt run --tag thread3 -n $NCPUS --main threadtest.py params.yaml nthreads=$NTHREADS ncpus=$NCPUS nslots=$NSLOTS solver=$SOLVER $@
+FIPY_SOLVERS=$SOLVER /data/guyer/miniconda3/envs/$CONDAENV/bin/mpiexec -n 1 smt run --tag thread3 -n $NCPUS --main threadtest.py params.yaml nthreads=$NTHREADS ncpus=$NCPUS nslots=$NSLOTS solver=$SOLVER $@
