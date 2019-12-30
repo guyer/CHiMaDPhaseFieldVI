0acd33f686d13bc02519101efb56d13bffa0daf9
Jonathan Guyer <guyer@nist.gov>
2019-12-30 16:02:39 -0500

    Enable different solvers

--- threadtest.sh
+++ threadtest.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 # invoke with
-# $ sbatch --array=0-5 --ntasks=32 threadtest.sh
+# $ sbatch --array=0-5 --ntasks=32 threadtest.sh <env> <solver>
 
 #SBATCH --job-name=threads
 #SBATCH --workdir=/data/guyer/CHiMaDPhaseFieldVI
 
-source activate petsc27
+CONDAENV=$1
+SOLVER=$2
+
+source activate $CONDAENV
 
 NTHREADS=$(( 2**SLURM_ARRAY_TASK_ID ))
 NSLOTS=$SLURM_NTASKS
 NCPUS=$(( NSLOTS / NTHREADS ))
 
+
 export OMP_NUM_THREADS=$NTHREADS
-FIPY_SOLVERS=trilinos /data/guyer/miniconda3/envs/petsc27/bin/mpiexec -n 1 smt run --tag trilinos -n $NCPUS --main threadtest.py params.yaml nthreads=$NTHREADS ncpus=$NCPUS nslots=$NSLOTS $@
+echo FIPY_SOLVERS=$SOLVER /data/guyer/miniconda3/envs/$CONDAENV/bin/mpiexec -n 1 smt run --tag thread3 -n $NCPUS --main threadtest.py params.yaml nthreads=$NTHREADS ncpus=$NCPUS nslots=$NSLOTS solver=$SOLVER $@
