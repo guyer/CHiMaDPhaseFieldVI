f8fa361b0ec96a0e88ef55a5105e911c486ed71b
Jonathan Guyer <guyer@nist.gov>
2020-01-02 14:16:27 -0500

    Add tag as parameter

--- threadtest.sh
+++ threadtest.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 # invoke with
-# $ sbatch --array=0-5 --ntasks=32 threadtest.sh <env> <solver>
+# $ sbatch --array=0-5 --ntasks=32 threadtest.sh <env> <solver> <tag>
 
 #SBATCH --job-name=threads
 #SBATCH --workdir=/data/guyer/CHiMaDPhaseFieldVI
 
 CONDAENV=$1
 SOLVER=$2
+TAG=$3
 
+shift
 shift
 shift
 
@@ -19,4 +21,4 @@ NCPUS=$(( NSLOTS / NTHREADS ))
 
 
 export OMP_NUM_THREADS=$NTHREADS
-FIPY_SOLVERS=$SOLVER mpiexec -n 1 smt run --tag thread3 -n $NCPUS --executable python --main threadtest.py params.yaml nthreads=$NTHREADS ncpus=$NCPUS nslots=$NSLOTS solver=$SOLVER $@
+FIPY_SOLVERS=$SOLVER mpiexec -n 1 smt run --tag $TAG -n $NCPUS --executable python --main threadtest.py params.yaml nthreads=$NTHREADS ncpus=$NCPUS nslots=$NSLOTS solver=$SOLVER $@
