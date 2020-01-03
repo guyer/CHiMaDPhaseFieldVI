72ad75afdd5c433e3d89a3f5a63e577d7e8fd769
Jonathan Guyer <guyer@nist.gov>
2020-01-03 06:45:08 -0500

    Fix yaml.load() call

--- sumatreant.py
+++ sumatreant.py
@@ -15,13 +15,12 @@ def load_sumatreant(project_name, path=None):
 
     prm_df = smt_df.parameters
 
-    with open(yamlfile, 'r') as f:
-        if hasattr(yaml, "FullLoader"):
-            # PyYAML 5.1 deprecated the plain yaml.load(input) function
-            # https://github.com/yaml/pyyaml/wiki/PyYAML-yaml.load(input)-Deprecation
-            prm_df = prm_df.apply(lambda x: pd.Series(yaml.load(x['content'], Loader=yaml.FullLoader)))
-        else:
-            prm_df = prm_df.apply(lambda x: pd.Series(yaml.load(x['content'])))
+    if hasattr(yaml, "FullLoader"):
+        # PyYAML 5.1 deprecated the plain yaml.load(input) function
+        # https://github.com/yaml/pyyaml/wiki/PyYAML-yaml.load(input)-Deprecation
+        prm_df = prm_df.apply(lambda x: pd.Series(yaml.load(x['content'], Loader=yaml.FullLoader)))
+    else:
+        prm_df = prm_df.apply(lambda x: pd.Series(yaml.load(x['content'])))
 
     smt_df.drop(columns=['parameters'])
 
